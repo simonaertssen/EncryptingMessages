4137e86708bae03be67a5b2d0e57737bcae37d66
Simon Aertssen <Simonaertssen@gmail.com>
2021-03-07 10:29:42 +0100

    Added a correct clock

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
+// g++ -std=c++11 -o main.o main.cpp simplesocket.cpp server.cpp client.cpp && ./main.o
 #include <iostream>
 #include "server.h"
 #include "client.h"
-
-#define PORT 8080
 
 int main(int argc, char *argv[]){
   std::cout << "Creating objects..." << std::endl;
--- server.cpp
+++ server.cpp
@@ -3,14 +3,13 @@
 #include <unistd.h>
 #include <ctime>
 
-
 Server::Server() : SimpleSocket(){
   std::cout << "Creating Server..." << std::endl;
   Address.sin_family = AF_INET;
   Address.sin_addr.s_addr = INADDR_ANY;
   Address.sin_port = htons(PORT);
   AddressLength = sizeof(Address);
-  bind(FileDescriptor, (struct sockaddr*)&Address, AddressLength);
+  // bind(FileDescriptor, (struct sockaddr*)&Address, AddressLength);
   // listen(FileDescriptor, MAX_CONNECTIONS);
   // ListenToID = accept(FileDescriptor, (struct sockaddr*)&Address, (socklen_t*)&AddressLength);
   ReadContinuously();
@@ -18,14 +17,14 @@ Server::Server() : SimpleSocket(){
 
 void Server::ReadContinuously(){
   std::cout << "Reading continuously .. " << std::endl;
-  int MAX_TIME = 5;
-  std::time_t start_time = std::time(nullptr), now = start_time;
+  std::clock_t start_time = std::clock(), now = std::clock();
+  double MAX_TIME = 0.01;
   std::cout << "start_time = " << start_time << std::endl;
-
-  while(now - start_time < MAX_TIME) {
+  while(((now - start_time)/(double)CLOCKS_PER_SEC) < MAX_TIME) {
     // read(ListenToID, MessageBuffer, BUFFER_SIZE);
-    std::cout << "now = " << now << std::endl;
-    now = std::time(nullptr);
+    std::cout << "diff = " << ((now - start_time)/(double)CLOCKS_PER_SEC) << std::endl;
+    now = std::clock();
+    // Wait
   }
 }
 
